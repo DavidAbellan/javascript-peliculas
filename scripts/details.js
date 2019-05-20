a014068309ef32adfcdf281f7c9e5d70b71192b1
Chane <davidabellan@rocketmail.com>
2019-05-20 17:05:51 +0200

    20/05Commit

--- scripts/details.js
+++ scripts/details.js
@@ -1,15 +1,22 @@
-import peliculas from './films.js'
+/* import peliculas from './films.js' */
 
 window.addEventListener('load', load);
 
-function load() {
-    const RUTA = 'http://image.tmdb.org/t/p/w185/';
-    let url = new URL(window.location.href);
-    let idpelicula = url.searchParams.get('IDpelicula').valueOf();
-    let pelicula = peliculas.find(a => idpelicula == a.id);
-    let divPelicula = document.querySelector('#contenedor');
-    let containerinfo = document.createElement('div');
-    containerinfo.innerHTML = `<img src="${RUTA}${pelicula.poster_path}"/> 
+const RUTA = 'http://image.tmdb.org/t/p/w185/'
+const API_URL = "https://api.themoviedb.org/3/"
+const API_KEY = "?api_key=80a1695fec74ccf4bb37c6b5c03ab6ee"
+const API_POPULAR_URL = 'movie/popular'
+
+async function load() {
+  let url = new URL(window.location.href);
+  let idpelicula = url.searchParams.get('IDpelicula').valueOf();
+   const RESULT = await axios.get(API_URL + API_POPULAR_URL + API_KEY)
+  let peliculas = RESULT.data.results
+ 
+  let pelicula = peliculas.find(a => idpelicula == a.id);
+  let divPelicula = document.querySelector('#contenedor');
+  let containerinfo = document.createElement('div');
+  containerinfo.innerHTML = `<img src="${RUTA}${pelicula.poster_path}"/> 
                                <div>
                               <h1>${pelicula.original_title}</h1 >
                               <p> ${pelicula.overview}</p> 
@@ -19,7 +26,7 @@ function load() {
                              <p id="fecha"> Fecha de Lanzamiento : ${pelicula.release_date}</p>
                                </div>`
 
-    divPelicula.appendChild(containerinfo);
+  divPelicula.appendChild(containerinfo);
 
 
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,103 +1,119 @@
- import peliculas from './films.js'
+ /* import peliculas from './films.js' */
 
-window.addEventListener('load', () => {
-         console.log('document loaded')
-         let cabecera = document.querySelector('#cabecera');
-         cabecera.innerHTML = '<h1>Movies Bazar </h1>';
-         let galeria = document.querySelector('#galeria');
-
-         document.querySelector('#btnRate').addEventListener('click', ordenarPorRate);
-         document.querySelector('#btnAlf').addEventListener('click', ordenarPorNombre);
-         document.querySelector('#btnLan').addEventListener('click', ordenarPorFecha);
-         let botonBuscar = document.querySelector('#btnBuscar');
-         botonBuscar.addEventListener('click', buscar)
-
-         const RUTA = 'http://image.tmdb.org/t/p/w185/';
-         imprimir(peliculas);
+ let peliculas;
+ const API_URL = "https://api.themoviedb.org/3/"
+ const API_KEY = "?api_key=80a1695fec74ccf4bb37c6b5c03ab6ee"
+ const API_POPULAR_URL = 'movie/popular'
+ const RUTA = 'http://image.tmdb.org/t/p/w185/'
 
 
-         function ordenarPorRate() {
 
-             let peliculasFiltradas = [];
+ let cabecera = document.querySelector('#cabecera');
+ let galeria;
 
-             peliculasFiltradas = peliculas.sort((p1, p2) => {
-                 return p2.vote_average - p1.vote_average
-             })
-             console.log(peliculasFiltradas);
+ document.querySelector('#btnRate').addEventListener('click', ordenarPorRate);
+ document.querySelector('#btnAlf').addEventListener('click', ordenarPorNombre);
+ document.querySelector('#btnLan').addEventListener('click', ordenarPorFecha);
+ document.querySelector('#btnBuscar').addEventListener('click', buscar);
 
-             imprimir(peliculasFiltradas);
 
+ window.addEventListener('load', load)
+
+ async function load() {
+    cabecera.innerHTML = '<h1>Movies Bazar </h1>';
+     let result = await axios.get(API_URL + API_POPULAR_URL + API_KEY)
+     peliculas = result.data.results
+     galeria = document.querySelector('#galeria');
+     imprimir(peliculas)
+
+
+
+ }
+
+ function ordenarPorRate() {
+
+     let peliculasFiltradas = [];
+
+
+
+     peliculasFiltradas = peliculas.sort((p1, p2) => {
+         return p2.vote_average - p1.vote_average
+     })
+
+
+     imprimir(peliculasFiltradas);
+
+
+ }
+
+ function ordenarPorNombre() {
+
+     let peliculasFiltradas = [];
+
+     peliculasFiltradas = peliculas.sort((p1, p2) => {
+         if (p1.title > p2.title) {
+             return 1;
+         } else if (p1.title < p2.title) {
+             return -1;
+         }
+         return 0;
+     })
+     console.log(peliculasFiltradas);
+
+     imprimir(peliculasFiltradas);
+
+
+ }
+
+ function ordenarPorFecha() {
+
+     let peliculasFiltradas = [];
+
+     peliculasFiltradas = peliculas.sort((p1, p2) => {
+         if (p1.release_date > p2.release_date) {
+             return 1;
+         } else if (p1.release_date < p2.release_date) {
+             return -1;
+         }
+         return 0;
+     })
+     console.log(peliculasFiltradas);
+
+     imprimir(peliculasFiltradas);
+ }
+
+ function imprimir(peliculas) {
+     document.querySelector('#galeria').innerHTML = '';
+     for (let pelicula of peliculas) {
+         let {
+             id: identificador,
+             poster_path: imagen,
+             title: titulo,
+             vote_average: puntuacion,
+             release_date: year,
+             overview: descripcion
+         } = pelicula;
+
+         let divPelicula = document.createElement('div');
+
+         function estrellas(p) {
+             let numero = '';
+             for (let i = 0; i <= puntuacion / 2; i++) {
+                 numero += '<i class="fas fa-star"></i>';
+             }
+             return numero;
+         }
+         let numeroEstrellas = estrellas(puntuacion);
+
+         let rotulo = ''
+         if (titulo.length > 30) {
+             rotulo = 'h6';
+         } else {
+             rotulo = 'h3';
 
          }
 
-         function ordenarPorNombre() {
-
-             let peliculasFiltradas = [];
-
-             peliculasFiltradas = peliculas.sort((p1, p2) => {
-                 if (p1.title > p2.title) {
-                     return 1;
-                 } else if (p1.title < p2.title) {
-                     return -1;
-                 }
-                 return 0;
-               })
-             console.log(peliculasFiltradas);
-
-             imprimir(peliculasFiltradas);
-
-
-         }
-
-         function ordenarPorFecha() {
-
-             let peliculasFiltradas = [];
-
-             peliculasFiltradas = peliculas.sort((p1, p2) => {
-                 if (p1.release_date > p2.release_date) {
-                     return 1;
-                 } else if (p1.release_date < p2.release_date) {
-                     return -1;
-                 }
-                 return 0;
-              })
-             console.log(peliculasFiltradas);
-
-             imprimir(peliculasFiltradas);
-           }
-
-         function imprimir(peliculas) {
-             document.querySelector('#galeria').innerHTML = '';
-             for (let pelicula of peliculas) {
-                 let {
-                     id: identificador,
-                     poster_path: imagen,
-                     title: titulo,
-                     vote_average: puntuacion,
-                     release_date: year,
-                     overview: descripcion
-                 } = pelicula;
-
-                 let divPelicula = document.createElement('div');
-
-                 function estrellas(p) {
-                     let numero = '';
-                     for (let i = 0; i <= puntuacion / 2; i++) {
-                         numero += '<i class="fas fa-star"></i>';
-                     }
-                     return numero;
-                 }
-                 let numeroEstrellas = estrellas(puntuacion);
-
-                 let rotulo = ''
-                 if (titulo.length > 30) {
-                     rotulo = 'h6';
-                 } else {
-                     rotulo = 'h3';
-
-                 }
-
-                 divPelicula.innerHTML = `   
+         divPelicula.innerHTML = `   
                                          <a href="/detalles.html?IDpelicula=${identificador}">
                                          <img title="${descripcion}" src="${RUTA}${imagen}">
                                          </a>
@@ -107,20 +123,17 @@ window.addEventListener('load', () => {
 
 
 
-                 galeria.appendChild(divPelicula);
-             }
-}
-
-         function buscar() {
-             let cajaTexto = document.querySelector('#busqueda');
-             let busqueda = cajaTexto.value;
-             busqueda = busqueda.toLowerCase();
-
-             let peliculasEncontradas = peliculas.filter(a => {
-                 return a.title.toLowerCase().includes(busqueda)
-             })
-             imprimir(peliculasEncontradas);
-         }
+         galeria.appendChild(divPelicula);
      }
+ }
 
- );
+ function buscar() {
+     let cajaTexto = document.querySelector('#busqueda');
+     let busqueda = cajaTexto.value;
+     busqueda = busqueda.toLowerCase();
+
+     let peliculasEncontradas = peliculas.filter(a => {
+         return a.title.toLowerCase().includes(busqueda)
+     })
+     imprimir(peliculasEncontradas);
+ }
